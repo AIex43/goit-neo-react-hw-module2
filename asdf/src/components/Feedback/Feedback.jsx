3bfa4721e2f0b69f48eedd11c30b3739b82a6f41
AIex43 <alex2612061@gmail.com>
2026-03-17 01:46:37 +0300

    ssasasas

---
+++ asdf/src/components/Feedback/Feedback.jsx
@@ -0,0 +1,19 @@
+import css from "./Feedback.module.css";
+
+export default function Feedback({
+  good,
+  neutral,
+  bad,
+  total,
+  positive,
+}) {
+  return (
+    <div className={css.stats}>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive: {positive}%</p>
+    </div>
+  );
+}
